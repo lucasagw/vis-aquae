c9d6198308dc7cbca84f8bdbf6f1bd57ca22630e
jamile08 <jamile1008@gmail.com>
2021-05-28 12:02:32 -0300

    Ajuste responsividade home_screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vis_aquae/core/app_colors.dart';
-import 'package:vis_aquae/shared/widgets/logo_with_background.dart';
+import 'package:vis_aquae/shared/widgets/app_logo.dart';
 import 'package:vis_aquae/shared/widgets/side_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,24 +14,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
+        width: media.width,
+        height: media.height,
+        alignment: Alignment.center,
         color: AppColors.backgroundScreen,
-        child: Column(
-          children: [
-            SizedBox(height: 150),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: SideBar(),
-            ),
-            SizedBox(height: 150),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                LogoWithBackgound(),
-              ],
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 100.h),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: SideBar(),
+              ),
+              SizedBox(height: 100.h),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AppLogo(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
